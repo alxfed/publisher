7c2f722c58aa89cac55d6d2fbb6a35f32865ca84
Alex Fedotov <alxfed@gmail.com>
2024-02-05 09:39:47 -0600

    working

--- util/openailog.py
+++ util/openailog.py
@@ -6,7 +6,18 @@ This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
 import json
+import jsonlines
+from time import sleep
 from  recorder import save_all_records
+from util.githublog import creupdate_repo, creupdate_file
+# import septem_circumstantiae as resources
+# import importlib.resources as impres
+#
+#
+# # https://importlib-resources.readthedocs.io/en/latest/using.html
+# with impres.files(resources).joinpath('resources/who.text').open('r', encoding='utf-8') as file:
+#     text = file.readline()
+#     all_text = file.read()
 
 
 def ingest_json(file_path):
@@ -17,11 +28,18 @@ def ingest_json(file_path):
 
 def extract_content(message):
     text = ''.join(message['content']['parts'])
+    text = text.replace('\\[','$$')
+    text = text.replace('\\]','$$')
+    text = text.replace('\\(','$')
+    text = text.replace('\\)','$')
     return text
 
 
 def process_system_message(message):
-    processed_message = message
+    processed_message = None
+    text = ''.join(message['content']['parts'])
+    if text != '':
+        processed_message = text
     return processed_message
 
 
@@ -48,7 +66,12 @@ def process_conversation(conversation):
                 author_name = config['user_name'] if author_name is None else author_name
                 record.append({"author": author_name, "utterance": result})
             elif author_role == 'system':
-                _ = process_system_message(present_message)
+                result = process_system_message(present_message)
+                if result is not None:
+                    author_name = config['system_name'] if author_name is None else author_name
+                    record.append({"author": author_name, "utterance": result})
+                else:
+                    pass
             elif author_role == 'assistant':
                 result = process_assistant_message(present_message)
                 author_name = config['assistant_name'] if author_name is None else author_name
@@ -61,28 +84,68 @@ def process_conversation(conversation):
     return record
 
 
+def make_md_file(agenda: dict, record_list: list):
+    md_text = f'# {agenda["topic"]}\n'
+
+    for record in record_list:
+        if record['author'] == agenda['user_name']:
+            md_text += f'\n<b>{record["author"]}:</b> {record["utterance"]}<br>'
+        elif record['author'] == agenda['assistant_name']:
+            md_text += f'\n> <b>{record["author"]}:</b> {record["utterance"]}<br>\n'
+        else:
+            print('Unexpected author')
+    return md_text
+
+
 def print_out(record):
     for utterance in record:
         print(utterance)
 
 
-def publish():
-    print('Publishing...')
+def publish(configuration: dict, record_list: list):
+    human_says = input('Branch name? ("Enter" for "main") ')
+    if human_says == '':
+        branch = 'main'
+    else:
+        branch = human_says
+    # Create the README.md file
+    with open('./agenda.json', 'r') as file:
+        agenda = json.load(file)
+    md_file = make_md_file(agenda=agenda, record_list=record_list)
+    # Creupdate the repository
+    organization = configuration['organization']
+    repository_object = creupdate_repo(repository_name=configuration['repository'],
+                                       description='Dialogue with an AI',
+                                       private=False)
+    sleep(10)
+    try:
+        print('Publishing...')
+        result = creupdate_file(repository=repository_object,
+                                file_path='./README.md',
+                                file_content=md_file,
+                                branch=branch)
+    except Exception as e:
+        print('failed ', e)
+    try:
+        result = creupdate_file(repository=repository_object,
+                                file_path='./agenda.json',
+                                file_content=json.dumps(agenda),
+                                branch=branch)
+    except Exception as e:
+        print('failed ', e)
 
 
 if __name__ == '__main__':
-    with open('/home/alxfed/Documents/Adler/Reasoning.txt', 'r') as file:
-        for line in file:
-            l = line
     config = ingest_json('./util/config.json')
-    conversations_list = ingest_json(f'{config["conversations_dir"]}conversations.json')
+    directory = config["conversations_dir"]
+    conversations_list = ingest_json(f'{directory}conversations.json')
     for conv in conversations_list:
         list_of_utterances = process_conversation(conv)
         decision = input('What should I do with it? (p - publish, s - save record,<enter> - skip, stop):  ')
         if decision == 'stop':
             break
         elif decision == 'p':
-            publish()
+            publish(configuration=config, record_list=list_of_utterances)
         elif decision == 's':
             save_all_records(list_of_utterances)
 
